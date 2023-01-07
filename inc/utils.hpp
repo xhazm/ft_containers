67f33baa9ab20e217642bb8b3e5a858805fb35d0
xhazm <lars.pfleiderfb@gmail.com>
2023-01-07 14:00:17 +0100

    added: equal and lexicographical_compare

--- inc/utils.hpp
+++ inc/utils.hpp
@@ -72,4 +72,59 @@ template<> struct is_integral_base<volatile signed char>: true_type {};
 template< class T >
 struct is_integral: is_integral_base <T> {};
 
+template< class InputIt1, class InputIt2, class BinaryPredicate >
+bool equal( InputIt1 first1,
+            InputIt1 last1,
+            InputIt2 first2,
+            BinaryPredicate p )
+{
+    for (; first1 != last1; ++first1, ++first2) {
+        if (!p(*first1, *first2)) {
+            return false;
+        }
+    }
+    return true;
+};
+
+template< class InputIt1, class InputIt2 >
+bool equal( InputIt1 first1, InputIt1 last1, InputIt2 first2 )
+{
+    for (; first1 != last1; ++first1, ++first2) {
+        if (*first1 == *first2) {
+            return false;
+        }
+    }
+    return true;
+};
+
+
+// Elements are compared using operator<
+template< class InputIt1, class InputIt2 >
+bool lexicographical_compare( InputIt1 first1, InputIt1 last1,
+                              InputIt2 first2, InputIt2 last2 )
+{
+    for (; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2)
+    {
+        if (*first1 < *first2)
+            return true;
+        if (*first2 < *first1)
+            return false;
+    }
+    return (first1 == last1) && (first2 != last2);
+}
+
+//Elements are compared using given binary comparison function comp
+template< class InputIt1, class InputIt2, class Compare >
+bool lexicographical_compare( InputIt1 first1, InputIt1 last1,
+                              InputIt2 first2, InputIt2 last2, Compare comp )
+{
+    for (; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2)
+    {
+        if (comp(*first1, *first2))
+            return true;
+        if (comp(*first2, *first1))
+            return false;
+    }
+    return (first1 == last1) && (first2 != last2);
+}
 }
