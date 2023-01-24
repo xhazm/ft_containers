f4762212fc86d1023c1e831f378e34f7f92faf1d
xhazm <lars.pfleiderfb@gmail.com>
2023-01-24 15:09:07 +0100

    added: create_node changed because end_

--- inc/avl.hpp
+++ inc/avl.hpp
@@ -41,31 +41,24 @@ namespace ft
         // typedef typename node_allocator_type::size_type                             size_type;
         typedef size_t                             size_type;
 
-        // typedef ft::vector_iterator< value_type >			iterator;
-        // typedef ft::vector_iterator< value_type >			const_iterator;
+        typedef ft::vector_iterator< value_type >			iterator;
+        typedef ft::vector_iterator< value_type >			const_iterator;
 
     private:
         value_allocator_type    value_alloc_;
         value_allocator_type     node_alloc_;
         // node_allocator_type     node_alloc_;
         node_pointer            root_;
-        node_pointer            begin_;
         node_pointer            end_;
         size_type               size_;
         value_compare           cmp_;
     
 
     public:
-        // avl_tree() : root_(NULL), size(0)
-        // {
-        //     begin_ = NULL;
-        //     end_ = NULL;
-        // }
         // avl_tree(const value_compare& cmp, const value_allocator_type value_alloc = value_allocator_type())
         avl_tree() : size_(0), cmp_(cmp), value_alloc_(value_alloc)
         {
             end_ = create_node_(value_type(), NULL);
-            begin_ = end_;
             root_ = end_;
             //init also node alloc? or is it auto inited
         }
@@ -76,8 +69,8 @@ namespace ft
         }
 
 /* =================				    Iterators   				================= */
-    iterator				begin()			{ return iterator(begin_); }
-    const_iterator			begin() const	{ return const_iterator(begin_); }
+    iterator				begin()			{ return iterator(root_); }
+    const_iterator			begin() const	{ return const_iterator(root_); }
     iterator				end()			{ return iterator(end_); }
     const_iterator			end() const		{ return const_iterator(end_); }
 
@@ -107,7 +100,6 @@ namespace ft
         if (pos == end_)
         {
             root_ = create_node_(value, NULL);
-            begin_ = root_;
             end_->parent = root_;
             return (ft::make_pair(iterator(root_), true))
         }
@@ -122,7 +114,7 @@ namespace ft
         }
         if(cmp_(value, pos->value)) //left
         {
-            node_pointer new_node = create_node_(value);
+            node_pointer new_node = create_node_(value, pos);
             pos->left = new_node;
             new_node->height = pos->height + 1;
             check_rule_violation_(new_node);
@@ -130,7 +122,7 @@ namespace ft
         }
         else if(cmp_(pos->value, value)) //right
         {
-            node_pointer new_node = create_node_(value);
+            node_pointer new_node = create_node_(value, pos);
             pos->right = new_node;
             new_node->height = pos->height + 1;
             check_rule_violation_(new_node);
@@ -143,7 +135,6 @@ namespace ft
     {
         clear_helper_(root_);
         root_ = end_;
-        begin_ = end_;
         end_->parent = NULL;
     }
 
@@ -267,19 +258,13 @@ namespace ft
         new_node->height_diff = 0; //get height diff function
         value_alloc_.construct(&new_node->value, value);
         ++size_;
-        if (parent == NULL)
-        {
-            root_ = new_node;
-            end_ = root_;
-            begin_ = root_;
-        }
         return new_node;
     }
 
     void delete_node_(node_pointer node)
     {
         if (node != end_node_)
-            --size_; //?
+            --size_;
         value_alloc_.destroy(&node->value);
         node_allocator.deallocate(node, 1);
     }
